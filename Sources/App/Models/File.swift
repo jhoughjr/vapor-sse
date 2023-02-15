b12fff29dda735e455bfadfe63785ccd08d66175
Jimmy Hough <jimmyhoughjr@me.com>
2023-02-15 07:13:05 -0600

    Added EventSourceConntection DTO

---
+++ Sources/App/Models/File.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  
+//
+//  Created by Jimmy Hough Jr on 2/15/23.
+//
+
+import Foundation
+
+struct EventSourceConnection:Codable {
+    let url:URL
+    let authToken:String
+}
